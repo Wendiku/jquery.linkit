a22e6822dc43160e308c264439926360727c8626
Wendiku <mangkeyou@gmail.com>
2017-12-27 17:53:52 +0800

    0.1.0 release

---
+++ jquery_linkit.js
@@ -0,0 +1,35 @@
+/*
+* name:   LinkIt
+* author: Wendiku
+* version: 0.10
+*license: MIT
+*/
+
+(function($){
+    $.fn.linkIt = function(options){
+        //default settings
+        var settings = $.extend({
+            href   :null,
+            text   :null,
+            target :'_self'
+        }, options);
+
+        //validation
+        if(settings.href == null){
+            console.log('You need an href option for LinkIt to work');
+            return this;
+
+        }
+
+        return this.each(function(){
+            var object = $(this);
+
+            if(settings.text == null){
+                settings.text == object.text();
+            }
+
+
+        object.wrap('<a target="'+settings.target+'" href="'+settings.href+'"></a>').text(settings.text);
+		});
+	}
+ }(jQuery));
